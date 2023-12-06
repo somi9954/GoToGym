9c7858c935cbc72c2a11fb9532f64e180ab81537
somi9954 <jolh999@gmail.com>
2023-12-06 16:27:15 +0900

    :memo:

--- front-app/src/containers/member/MemberListContainer.js
+++ front-app/src/containers/member/MemberListContainer.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import MemberList from '../../components/member/MemberList';
 import { getLoginInfo } from '../../api/member/login';
 
 const MemberListContainer = () => {
   const [members, setMembers] = useState([]);
   const [error, setError] = useState(null);
+  const history = useNavigate();
 
   useEffect(() => {
-    // getLoginInfo 함수를 호출하여 데이터를 받아오기
+
     getLoginInfo()
       .then((userInfo) => {
-        // 데이터가 배열이 아니라면 배열로 변환
+
         const membersArray = Array.isArray(userInfo) ? userInfo : [userInfo];
         setMembers(membersArray);
       })
       .catch((error) => {
         setError(error.message);
-        alert(error.message); // 에러 메시지를 alert으로 표시
-        history.goBack(); // 뒤로가기
+        alert(error.message);
+        history.goBack();
       });
   }, [history]);
 
